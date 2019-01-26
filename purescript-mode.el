518af4465c76da239fae3ce9c2bda3c35735c28d
justinwoo <moomoowoo@gmail.com>
2019-01-26 21:50:41 +0200

    add more characters to words so that autocomplete suggestions from psc-ide are smarter

--- purescript-mode.el
+++ purescript-mode.el
@@ -39,6 +39,8 @@
 (setq
  purescript-mode-syntax-table
  (let ((table (make-syntax-table)))
+   (modify-syntax-entry ?_  "w" table)
+   (modify-syntax-entry ?.  "w" table)
    (modify-syntax-entry ?\{  "(}1nb" table)
    (modify-syntax-entry ?\}  "){4nb" table)
    (modify-syntax-entry ?-  "_ 123" table)
